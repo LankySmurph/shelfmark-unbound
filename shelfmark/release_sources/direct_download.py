b840c01256e425e67215b89039f99774df682ad4
LankySmurph <lankysmurph@smurphey.com>
2026-08-22 16:02:33 -0500

    Update direct_download.py
    Harcoded libgen search.

--- shelfmark/release_sources/direct_download.py
+++ shelfmark/release_sources/direct_download.py
@@ -648,10 +648,10 @@ def search_books(query: str, filters: SearchFilters) -> list[BrowseRecord]:
 
     """
     # --- SHELFMARK-UNBOUND INJECTION ---
-    from shelfmark.core.config import config
-    if config.get("ENABLE_LIBGEN_DIRECT_SEARCH", False):
-        from shelfmark.release_sources.libgen_search import search_libgen
-        return search_libgen(query, filters)
+    # from shelfmark.core.config import config
+    # if config.get("ENABLE_LIBGEN_DIRECT_SEARCH", False):
+    from shelfmark.release_sources.libgen_search import search_libgen
+    return search_libgen(query, filters)
     # -----------------------------------
     
     query_html = quote(query)
